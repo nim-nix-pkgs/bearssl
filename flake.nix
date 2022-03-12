3f60ee93bbc3fd921fa943ae57bdceb0fb334d8b
hugosenari <hugosenari@gmail.com>
2022-03-12 00:45:13 -0300

    chore: re index bearssl

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Bindings to BearSSL library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."bearssl-master".url = "path:./master";
+  inputs."bearssl-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bearssl-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
